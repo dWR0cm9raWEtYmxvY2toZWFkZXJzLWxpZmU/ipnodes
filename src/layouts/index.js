1e1249e0346a1274dd747c532338b1ff2db3d973
mercury <udtrokia@163.com>
2018-02-27 01:30:13 +0800

    helmet

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,7 +9,7 @@ import Footer from '../components/Footer'
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="IPNodes"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
