342bbcb5dd1a2dfe60bc80e5d7a765a2e851606d
mercury <udtrokia@163.com>
2018-02-28 15:07:40 +0800

    font error

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ import './index.css'
 const Footer = () => (
 		<div id="footer">
 				<div className="cpn">
-						<h3 className="flt">©️  2018 - 晋莱(北京)科技有限公司</h3>
+						<h3 className="flt">©️  2018 - 晋莱(北京)科技有限公司 http://ipnodes.io</h3>
 						<h3 className="flt">讲师招聘 | 人才招聘 | 企业合作 | 常见问题 | 友情链接</h3>						
 				</div>
 		</div>
